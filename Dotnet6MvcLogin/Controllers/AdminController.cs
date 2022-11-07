45f7c8f3544370e89369a57dfe0dbe5fba6cd68d
Mama Moh <mamamohammed31@gmail.com>
2022-11-07 08:21:57 +0300

    edit 3

--- Dotnet6MvcLogin/Controllers/AdminController.cs
+++ Dotnet6MvcLogin/Controllers/AdminController.cs
@@ -1,20 +1,51 @@
-﻿using Dotnet6MvcLogin.ViewModel;
+﻿using Dotnet6MvcLogin.Models.Domain;
+using Dotnet6MvcLogin.Models.DTO;
+using Dotnet6MvcLogin.Repositories.Abstract;
+using Dotnet6MvcLogin.ViewModel;
+using Dotnet6MvcLogin.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Dotnet6MvcLogin.Controllers
 {
     
-    [Authorize(Roles ="admin")]
+   // [Authorize(Roles ="admin")]
     public class AdminController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly IUserAuthenticationService _authService;
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, IUserAuthenticationService authService)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
+            _authService = authService;
+        }
+        [Authorize(Roles = "admin")]
+        public async Task <IActionResult> Users()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            List<UserViewModel> result = new List<UserViewModel>();
+            foreach (var user in users)
+            {
+                var userViewModel = new UserViewModel()
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Roles = _userManager.GetRolesAsync(user).Result,
+                };
+                result.Add(userViewModel);
+            }
+
+            return View(result);
         }
         public IActionResult ListAllRoles()
         {
@@ -27,23 +58,58 @@ namespace Dotnet6MvcLogin.Controllers
         {
             return View();
         }
-        [HttpPost]
-        public async Task<IActionResult> AddRole(AddViewModel model)
+        public IActionResult Registration()
         {
-            if (ModelState.IsValid)
-            {
-                IdentityRole identityRole = new()
-                {
-                    Name = model.RoleName
-                };
-                var result =await _roleManager.CreateAsync(identityRole);
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("ListAllRoles");
-                }
-               }
-            
-            return View(model);
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registration(RegistrationModel model)
+        {
+            if (!ModelState.IsValid) { return View(model); }
+            model.Role = "user";
+            var result = await this._authService.RegisterAsync(model);
+            TempData["msg"] = result.Message;
+            return RedirectToAction(nameof(Registration));
+        }
+
+      
+        //[AllowAnonymous]
+        //public async Task<IActionResult> RegisterAdmin()
+        //{
+        //    RegistrationModel model = new RegistrationModel
+        //    {
+        //        Username="admin",
+        //        Email="admin@gmail.com",
+        //        FirstName="John",
+        //        LastName="Doe",
+        //        Password="Admin@12345#"
+        //    };
+        //    model.Role = "admin";
+        //    var result = await this._authService.RegisterAsync(model);
+        //    return Ok(result);
+        //}
+        [Authorize]
+        public IActionResult Account()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var result = await _authService.ChangePasswordAsync(model, User.Identity.Name);
+            TempData["msg"] = result.Message;
+            return RedirectToAction(nameof(ChangePassword));
         }
 
     }
---
+++ Dotnet6MvcLogin/Models/DTO/UpdateModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dotnet6MvcLogin.Models.DTO
+{
+    public class UpdateModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+       public string? ProfilePicture { get; set; }
+    }
+}
